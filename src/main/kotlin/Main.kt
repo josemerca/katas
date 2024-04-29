f7d582c6c7337dba05ff3efe8046351db1869f11
josemerca <jpereag@mercadona.es>
2024-04-29 15:24:36 +0200

    mo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,22 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
-    var n: Int = 5
-    var w: Int = 2
-    println(sum(n, w))
-    n = 6
-    println(n)
+    println("FizzBuzz Kata!")
+    for (i in 1..100) {
+        if ((i.mod(5) == 0) && (i.mod(3) == 0)) {
+            println("FizzBuzz")
+        }
+        else if (i.mod(3) == 0) {
+            println("Fizz")
+        }
+        else if (i.mod(5) == 0) {
+            println("Buzz")
+        }
+        else {
+            println(i)
+        }
+    }
+
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
-}
-
-fun sum(a: Int, b: Int) : Int {
-    return a + b
 }
